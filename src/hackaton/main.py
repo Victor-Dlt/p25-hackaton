58f844ada6e94c6b8f1b4ea25a23350a3340d534
Victor Duault <victorduault@icloud.com>
2026-01-30 10:20:50 +0100

    modif classe client

--- src/hackaton/main.py
+++ src/hackaton/main.py
@@ -1,3 +1,13 @@
+import pandas as pd
+import numpy as np
+from .Client import Client
+
 
 def main() -> None:
-    pass
+    df= pd.read_csv("sujet-9-clients.csv")
+    df=df.to_numpy()
+
+    C = []
+    for ligne in df :
+        client = Client(ligne[0], ligne[1],ligne[2],ligne[3],ligne[4])
+        C.append(client)
